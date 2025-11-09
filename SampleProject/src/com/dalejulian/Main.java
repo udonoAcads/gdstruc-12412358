1b5c18a750cf5717f3eda919ec2833a3b5107be3
udonoAnythin <ocampoeronbenito@gmail.com>
2025-11-09 16:33:08 +0800

    Added Sample Project

---
+++ SampleProject/src/com/dalejulian/Main.java
@@ -0,0 +1,20 @@
+package com.dalejulian;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello " + System.getProperty("user.name"));
+
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+
+
+    }
+}
